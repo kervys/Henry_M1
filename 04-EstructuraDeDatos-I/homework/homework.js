980c0133df76a1db32ee29f3984690d10e4a7fe1
unknown <kervysjulian@gmail.com>
2022-10-07 14:48:28 -0400

    Estructura de Datos I

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -3,9 +3,13 @@
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
 
-nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
+nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural,
+su factorial (representado como n!) es el producto de n por todos los números naturales menores que él
+y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente,
+ como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia
+ será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
 
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
@@ -15,10 +19,16 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if (n === 0 || n === 1) return 1;
+  if(n<0) return "No es posible procesar Factorial";
+  return n = n * nFactorial(n -1);
 }
 
 function nFibonacci(n) {
-}
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return nFibonacci(n-1) + nFibonacci(n -2);
+  }
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -30,8 +40,26 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-
+  this.queue = [];
 }
+
+Queue.prototype.enqueue = function (val){
+this.queue.push(val);
+}
+
+Queue.prototype.dequeue = function (){
+ return this.queue.shift();
+}
+
+Queue.prototype.size = function (){ 
+ return this.queue.length;
+}
+
+
+Queue.prototype.show = function () {
+console.log(this.queue);
+}
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
